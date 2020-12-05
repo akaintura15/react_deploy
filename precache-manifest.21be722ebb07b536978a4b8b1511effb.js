b51c52c4cde53c859b60a67ae52bc6aca8b50f5b
akaintura15 <akaintura15@gmail.com>
2020-12-06 00:43:56 +0530

    Updates

---
+++ precache-manifest.21be722ebb07b536978a4b8b1511effb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ee901bd07135ffa57aa6432376e7309",
+    "url": "/react_deploy/index.html"
+  },
+  {
+    "revision": "7cb199d6be5d8078f1fe",
+    "url": "/react_deploy/static/css/main.4823d997.chunk.css"
+  },
+  {
+    "revision": "d09401dd82f4d495427a",
+    "url": "/react_deploy/static/js/2.ad5aac3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_deploy/static/js/2.ad5aac3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cb199d6be5d8078f1fe",
+    "url": "/react_deploy/static/js/main.ff0249cd.chunk.js"
+  },
+  {
+    "revision": "a2471aa1535022e6c8ca",
+    "url": "/react_deploy/static/js/runtime-main.eac100e7.js"
+  }
+]);
